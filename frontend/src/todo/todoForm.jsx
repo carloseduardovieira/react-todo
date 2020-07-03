525ee1b264125fb000dbca21bde7ddd647410b13
Carlos Eduardo Vieira <dullvieira@gmail.com>
2020-07-02 21:28:15 -0300

    feature: lidando com atalhos do teclado

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,33 +2,44 @@ import React from 'react';
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
 
-export default (props) => (
-  <div role="form" className="todoForm">
-    <Grid cols="12 9 10 10">
-      <input 
-        type="text" 
-        id="description"
-        className="form-control"
-        placeholder="Adicione uma tarefa"
-        onChange={props.handleChange}
-        value={props.description}  />
-    </Grid>
-    
-    <Grid cols="12 3 2 2">
-      <IconButton 
-        style="primary" 
-        icon="plus" 
-        onClick={props.handleAdd} />
+export default (props) => {
+  const keyHandler = (e) => {
+    if ( e.key === 'Enter' ) {
+      e.shiftKey ? props.handleSearch() : props.handleAdd();
+    } else if ( e.key === 'Escape' ) {
+      props.handleClear();
+    }
+  }
 
-      <IconButton 
-        style="info" 
-        icon="search" 
-        onClick={props.handleSearch} />
+  return (
+    <div role="form" className="todoForm">
+      <Grid cols="12 9 10 10">
+        <input 
+          type="text" 
+          id="description"
+          className="form-control"
+          placeholder="Adicione uma tarefa"
+          onChange={props.handleChange}
+          value={props.description}
+          onKeyUp={keyHandler} />
+      </Grid>
+      
+      <Grid cols="12 3 2 2">
+        <IconButton 
+          style="primary" 
+          icon="plus" 
+          onClick={props.handleAdd} />
 
-      <IconButton 
-        style="default" 
-        icon="close" 
-        onClick={props.handleClear} />
-    </Grid>
-  </div>
-);
+        <IconButton 
+          style="info" 
+          icon="search" 
+          onClick={props.handleSearch} />
+
+        <IconButton 
+          style="default" 
+          icon="close" 
+          onClick={props.handleClear} />
+      </Grid>
+    </div>
+  );
+}
